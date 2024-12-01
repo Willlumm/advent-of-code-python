3d39908e889ee2f7144fb3bfbc74cd6113e1efeb
William Lumme <80401792+Willlumm@users.noreply.github.com>
2024-12-01 23:36:56 +0000

    refactor: zip strict

--- aoc/utils.py
+++ aoc/utils.py
@@ -12,7 +12,7 @@ def read_input_file(filepath: str) -> Table:
 def calculate_total_distance(list1: list[int], list2: list[int]) -> int:
     return sum(
         abs(item1 - item2)
-        for item1, item2 in zip(sorted(list1), sorted(list2), strict=False)
+        for item1, item2 in zip(sorted(list1), sorted(list2), strict=True)
     )
 
 
