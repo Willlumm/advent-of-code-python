5b22d75bef5a7a8eb5eb8e05fffb6bbebced4e2c
William Lumme <80401792+Willlumm@users.noreply.github.com>
2024-12-01 23:00:25 +0000

    refactor: tidy tests

--- tests/test_table.py
+++ tests/test_table.py
@@ -6,20 +6,19 @@ from aoc.table import Table
 
 
 def test_cast_column() -> None:
-    input_table = Table([["1"], ["2"], ["3"]])
+    table = Table([["1"], ["2"], ["3"]])
     expected = Table([[1], [2], [3]])
-    input_table.cast_column(0, int)
-    assert input_table == expected
+    table.cast_column(0, int)
+    assert table == expected
 
 
 @pytest.mark.parametrize(
-    argnames=("input_column_index", "expected"),
+    argnames=("column_index", "expected"),
     argvalues=(
         pytest.param(0, ["foo", "bar", "baz"], id="column 0"),
         pytest.param(1, [1, 2, 3], id="column 1"),
     ),
 )
-def test_get_column(input_column_index: int, expected: list[Any]) -> None:
-    input_table = Table([["foo", 1], ["bar", 2], ["baz", 3]])
-    actual = input_table.get_column(input_column_index)
-    assert actual == expected
+def test_get_column(column_index: int, expected: list[Any]) -> None:
+    table = Table([["foo", 1], ["bar", 2], ["baz", 3]])
+    assert table.get_column(column_index) == expected
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,18 +2,15 @@ from aoc.utils import calculate_similarity, calculate_total_distance, read_input
 
 
 def test_read_input_file() -> None:
-    input_filepath = "tests/data/2024_01"
     expected = [["3", "4"], ["4", "3"], ["2", "5"], ["1", "3"], ["3", "9"], ["3", "3"]]
-    actual = read_input_file(input_filepath)
-    assert actual == expected
+    assert read_input_file("tests/data/2024_01") == expected
 
 
 def test_calculate_total_distance() -> None:
     input_list1 = [3, 4, 2, 1, 3, 3]
     input_list2 = [4, 3, 5, 3, 9, 3]
     expected = 11
-    actual = calculate_total_distance(input_list1, input_list2)
-    assert actual == expected
+    assert calculate_total_distance(input_list1, input_list2) == expected
 
 
 def test_calculate_similarity() -> None:
