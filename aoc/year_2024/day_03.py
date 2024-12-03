be80cf516cdcc88e019fa545bc959bea609741ce
William Lumme <80401792+Willlumm@users.noreply.github.com>
2024-12-03 19:17:50 +0000

    feat: day 03 part 2

--- aoc/year_2024/day_03.py
+++ aoc/year_2024/day_03.py
@@ -1,4 +1,5 @@
 import re
+from itertools import pairwise
 from pathlib import Path
 
 INPUT_FILEPATH = "data/2024_03"
@@ -9,12 +10,31 @@ def _prepare_data(filepath: str) -> str:
         return file.read()
 
 
-def part1(filepath: str) -> int:
+def _filter_data(data: str) -> str:
+    return "".join(
+        data_section
+        for instruction, data_section in pairwise(
+            re.split(r"(do(?:n't)?\(\))", "do()" + data)
+        )
+        if instruction == "do()"
+    )
+
+
+def _sum_muls(data: str) -> int:
     return sum(
         int(match[1]) * int(match[2])
-        for match in re.finditer(r"mul\(([0-9]+),([0-9]+)\)", _prepare_data(filepath))
+        for match in re.finditer(r"mul\(([0-9]+),([0-9]+)\)", data)
     )
+
+
+def part1(filepath: str) -> int:
+    return _sum_muls(_prepare_data(filepath))
+
+
+def part2(filepath: str) -> int:
+    return _sum_muls(_filter_data(_prepare_data(filepath)))
 
 
 if __name__ == "__main__":
     print(f"Part 1: {part1(INPUT_FILEPATH):>20}")
+    print(f"Part 2: {part2(INPUT_FILEPATH):>20}")
--- tests/year_2024/test_day_03.py
+++ tests/year_2024/test_day_03.py
@@ -1,13 +1,29 @@
-from aoc.year_2024.day_03 import _prepare_data, part1
+from aoc.year_2024.day_03 import _filter_data, _prepare_data, _sum_muls, part1, part2
 
 
 def test__prepare_data() -> None:
     expected = (
         r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
     )
-    assert _prepare_data("tests/data/2024_03") == expected
+    assert _prepare_data("tests/data/2024_03_1") == expected
+
+
+def test__filter_data() -> None:
+    data = r"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    assert _filter_data(data) == r"xmul(2,4)&mul[3,7]!^?mul(8,5))"
+
+
+def test__sum_muls() -> None:
+    data = r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    expected = 161
+    assert _sum_muls(data) == expected
 
 
 def test_part1() -> None:
     expected = 161
-    assert part1("tests/data/2024_03") == expected
+    assert part1("tests/data/2024_03_1") == expected
+
+
+def test_part2() -> None:
+    expected = 48
+    assert part2("tests/data/2024_03_2") == expected
