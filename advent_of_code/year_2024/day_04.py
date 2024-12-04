9784174b6f8b85cdccf872c11bf31186c1dafba9
William Lumme <80401792+Willlumm@users.noreply.github.com>
2024-12-04 23:29:42 +0000

    feat: day 04 part 2

--- advent_of_code/year_2024/day_04.py
+++ advent_of_code/year_2024/day_04.py
@@ -2,26 +2,50 @@ from pathlib import Path
 
 INPUT_FILEPATH = "data/2024_04"
 
+type Coord = tuple[int, int]
+type Pattern = tuple[Coord, ...]
+
 
 def _read_input(filepath: str) -> list[str]:
     with Path(filepath).open() as file:
         return file.read().split("\n")
 
 
-def _search_grid(data: list[str]) -> int:
+def _generate_ray_patterns() -> list[Pattern]:
+    """
+    S  S  S
+     A A A
+      MMM
+    SAMXMAS
+      MMM
+     A A A
+    S  S  S
+    """
+    dxys = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+    return [tuple((i * dx, i * dy) for i in range(4)) for dx, dy in dxys]
+
+
+def _generate_x_patterns() -> list[Pattern]:
+    """
+    M M     S M     S S     M S
+     A       A       A       A
+    S S     S M     M M     M S
+    """
+    dxys = ((-1, -1), (-1, 1), (1, 1), (1, -1))
+    return [((0, 0), *(dxys[(i + j) % 4] for i in range(4))) for j in range(4)]
+
+
+def _search_grid(data: list[str], chars: str, patterns: list[Pattern]) -> int:
     total = 0
     x_max = len(data)
-    dxys = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
     for x, row in enumerate(data):
         y_max = len(row)
         for y, _ in enumerate(row):
-            for dx, dy in dxys:
-                for i, char in enumerate("XMAS"):
-                    xi = x + i * dx
-                    yi = y + i * dy
-                    if not (x_max > xi >= 0 and y_max > yi >= 0):
+            for pattern in patterns:
+                for char, (dx, dy) in zip(chars, pattern, strict=False):
+                    if not (x_max > x + dx >= 0 and y_max > y + dy >= 0):
                         break
-                    if data[xi][yi] != char:
+                    if data[x + dx][y + dy] != char:
                         break
                 else:
                     total += 1
@@ -29,8 +53,13 @@ def _search_grid(data: list[str]) -> int:
 
 
 def part1(filepath: str) -> int:
-    return _search_grid(_read_input(filepath))
+    return _search_grid(_read_input(filepath), "XMAS", _generate_ray_patterns())
+
+
+def part2(filepath: str) -> int:
+    return _search_grid(_read_input(filepath), "AMMSS", _generate_x_patterns())
 
 
 if __name__ == "__main__":
     print(f"Part 1: {part1(INPUT_FILEPATH):>20}")
+    print(f"Part 2: {part2(INPUT_FILEPATH):>20}")
--- tests/year_2024/test_day_04.py
+++ tests/year_2024/test_day_04.py
@@ -1,6 +1,13 @@
 import pytest
 
-from advent_of_code.year_2024.day_04 import _read_input, _search_grid, part1
+from advent_of_code.year_2024.day_04 import (
+    _generate_ray_patterns,
+    _generate_x_patterns,
+    _read_input,
+    _search_grid,
+    part1,
+    part2,
+)
 
 TEST_DATA_FILEPATH = "tests/data/2024_04"
 
@@ -25,11 +32,40 @@ def test__read_input(data: list[str]) -> None:
     assert _read_input(TEST_DATA_FILEPATH) == data
 
 
+def test__generate_ray_patterns() -> None:
+    expected = [
+        ((0, 0), (-1, -1), (-2, -2), (-3, -3)),
+        ((0, 0), (-1, 0), (-2, 0), (-3, 0)),
+        ((0, 0), (-1, 1), (-2, 2), (-3, 3)),
+        ((0, 0), (0, -1), (0, -2), (0, -3)),
+        ((0, 0), (0, 1), (0, 2), (0, 3)),
+        ((0, 0), (1, -1), (2, -2), (3, -3)),
+        ((0, 0), (1, 0), (2, 0), (3, 0)),
+        ((0, 0), (1, 1), (2, 2), (3, 3)),
+    ]
+    assert _generate_ray_patterns() == expected
+
+
+def test__generate_x_patterns() -> None:
+    expected = [
+        ((0, 0), (-1, -1), (-1, 1), (1, 1), (1, -1)),
+        ((0, 0), (-1, 1), (1, 1), (1, -1), (-1, -1)),
+        ((0, 0), (1, 1), (1, -1), (-1, -1), (-1, 1)),
+        ((0, 0), (1, -1), (-1, -1), (-1, 1), (1, 1)),
+    ]
+    assert _generate_x_patterns() == expected
+
+
 def test__search_grid(data: list[str]) -> None:
     expected = 18
-    assert _search_grid(data) == expected
+    assert _search_grid(data, "XMAS", _generate_ray_patterns()) == expected
 
 
 def test_part1() -> None:
     expected = 18
     assert part1(TEST_DATA_FILEPATH) == expected
+
+
+def test_part2() -> None:
+    expected = 9
+    assert part2(TEST_DATA_FILEPATH) == expected
