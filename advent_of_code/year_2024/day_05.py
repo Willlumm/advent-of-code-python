9f3066c2be0704672d60249f203bc89ba230ea50
William Lumme <80401792+Willlumm@users.noreply.github.com>
2024-12-05 23:50:29 +0000

    feat: day 05 part 2

--- advent_of_code/year_2024/day_05.py
+++ advent_of_code/year_2024/day_05.py
@@ -1,21 +1,36 @@
-from collections import defaultdict
 from pathlib import Path
-from typing import Any, Self
+from typing import Self
 
 INPUT_FILEPATH = "data/2024_05"
 
 
-class PagesDict(defaultdict[int, set[int]]):
-    def __init__(self, *args: dict[Any, Any]) -> None:
-        super().__init__(set, *args)
-
+class PageDict(dict[int, set[int]]):
     @classmethod
     def from_str(cls, data: str) -> Self:
         pages = cls()
         for row in data.split("\n"):
-            required_page_number, page_number = (int(n) for n in row.split("|"))
-            pages[page_number].add(required_page_number)
+            required_number, number = (int(n) for n in row.split("|"))
+            if required_number not in pages:
+                pages[required_number] = set()
+            if number not in pages:
+                pages[number] = set()
+            pages[number].add(required_number)
         return pages
+
+    def simplify(self, numbers: list[int]) -> Self:
+        return type(self)(
+            {
+                number: required_numbers & set(numbers)
+                for number, required_numbers in self.items()
+                if number in numbers
+            }
+        )
+
+    def get_free_page_number(self) -> int:
+        for number, required_numbers in self.items():
+            if not required_numbers:
+                return number
+        raise IndexError
 
 
 class Update(list[int]):
@@ -27,26 +42,41 @@ class Update(list[int]):
     def from_str(cls, data: str) -> Self:
         return cls(int(n) for n in data.split(","))
 
-    def is_valid(self, pages: PagesDict) -> bool:
-        illegal_pages = set()
-        for page_number in self:
-            if page_number in illegal_pages:
+    def is_ordered(self, pages: PageDict) -> bool:
+        illegal_numbers = set()
+        for number in self:
+            if number in illegal_numbers:
                 return False
-            illegal_pages |= pages[page_number]
+            illegal_numbers |= pages[number]
         return True
+
+    def order(self, pages: PageDict) -> Self:
+        new_order = type(self)()
+        remaining_numbers = list(self)
+        for _ in range(len(self)):
+            pages = pages.simplify(remaining_numbers)
+            free_number = pages.get_free_page_number()
+            new_order.append(free_number)
+            remaining_numbers.remove(free_number)
+        return new_order
 
 
 class UpdateList(list[Update]):
     @property
-    def sum_middles(self) -> int:
+    def sum_of_middles(self) -> int:
         return sum(update.middle for update in self)
 
     @classmethod
     def from_str(cls, data: str) -> Self:
         return cls(Update.from_str(row) for row in data.split("\n"))
 
-    def filter(self, pages: PagesDict, *, valid: bool = True) -> Self:
-        return type(self)(update for update in self if update.is_valid(pages) is valid)
+    def filter(self, pages: PageDict, *, ordered: bool = True) -> Self:
+        return type(self)(
+            update for update in self if update.is_ordered(pages) is ordered
+        )
+
+    def order_all(self, pages: PageDict) -> Self:
+        return type(self)(update.order(pages) for update in self)
 
 
 def _read_input(filepath: str) -> str:
@@ -61,11 +91,19 @@ def _split_input(data: str) -> list[str]:
 def part1(filepath: str) -> int:
     data = _read_input(filepath)
     page_order_data, update_data = _split_input(data)
-    pages = PagesDict.from_str(page_order_data)
+    pages = PageDict.from_str(page_order_data)
     updates = UpdateList.from_str(update_data)
-    valid_updates = updates.filter(pages, valid=True)
-    return valid_updates.sum_middles
+    return updates.filter(pages, ordered=True).sum_of_middles
+
+
+def part2(filepath: str) -> int:
+    data = _read_input(filepath)
+    page_order_data, update_data = _split_input(data)
+    pages = PageDict.from_str(page_order_data)
+    updates = UpdateList.from_str(update_data)
+    return updates.filter(pages, ordered=False).order_all(pages).sum_of_middles
 
 
 if __name__ == "__main__":
     print(f"Part 1: {part1(INPUT_FILEPATH):>20}")
+    print(f"Part 2: {part2(INPUT_FILEPATH):>20}")
--- tests/year_2024/test_day_05.py
+++ tests/year_2024/test_day_05.py
@@ -3,11 +3,12 @@ from textwrap import dedent
 import pytest
 
 from advent_of_code.year_2024.day_05 import (
-    PagesDict,
+    PageDict,
     Update,
     UpdateList,
     _read_input,
     _split_input,
+    part2,
 )
 
 TEST_DATA_FILEPATH = "tests/data/2024_05"
@@ -87,16 +88,17 @@ def update_data() -> str:
 
 
 @pytest.fixture
-def pages() -> PagesDict:
-    return PagesDict(
+def pages() -> PageDict:
+    return PageDict(
         {
+            47: {97, 75},
             53: {97, 75, 61, 47},
+            97: set(),
             13: {97, 75, 47, 61, 53, 29},
             61: {97, 75, 47},
-            47: {97, 75},
-            29: {97, 75, 47, 53, 61},
             75: {97},
-        },
+            29: {97, 75, 47, 53, 61},
+        }
     )
 
 
@@ -114,6 +116,17 @@ def updates() -> UpdateList:
     )
 
 
+@pytest.fixture
+def unordered_updates() -> UpdateList:
+    return UpdateList(
+        [
+            Update([75, 97, 47, 61, 53]),
+            Update([61, 13, 29]),
+            Update([97, 13, 75, 29, 47]),
+        ]
+    )
+
+
 def test__read_input(data: str) -> None:
     assert _read_input(TEST_DATA_FILEPATH) == data
 
@@ -122,20 +135,56 @@ def test__split_input(data: str, page_order_data: str, update_data: str) -> None
     assert _split_input(data) == [page_order_data, update_data]
 
 
-def test__parse_page_data(page_order_data: str, pages: PagesDict) -> None:
-    assert PagesDict.from_str(page_order_data) == pages
+class TestPageDict:
+    def test_from_str(self, page_order_data: str, pages: PageDict) -> None:
+        print(PageDict.from_str(page_order_data))
+        assert PageDict.from_str(page_order_data) == pages
+
+    def test_get_free_number(self, pages: PageDict) -> None:
+        assert pages.get_free_page_number() == 97
+
+    def test_simplify(self, pages: PageDict) -> None:
+        expected = PageDict(
+            {
+                47: {75},
+                53: {75, 61, 47},
+                61: {75, 47},
+                75: set(),
+                29: {75, 47, 53, 61},
+            }
+        )
+        assert pages.simplify([75, 47, 61, 53, 29]) == expected
 
 
-def test__parse_update_data(update_data: str, updates: UpdateList) -> None:
-    assert UpdateList.from_str(update_data) == updates
+class TestUpdateList:
+    def test_from_str(self, update_data: str, updates: UpdateList) -> None:
+        assert UpdateList.from_str(update_data) == updates
+
+    def test_filter(
+        self, updates: UpdateList, unordered_updates: UpdateList, pages: PageDict
+    ) -> None:
+        assert updates.filter(pages, ordered=False) == unordered_updates
+
+    def test_order_all(self, unordered_updates: UpdateList, pages: PageDict) -> None:
+        expected = UpdateList(
+            [
+                Update([97, 75, 47, 61, 53]),
+                Update([61, 29, 13]),
+                Update([97, 75, 47, 29, 13]),
+            ]
+        )
+        assert unordered_updates.order_all(pages) == expected
+
+    def test_sum_of_middles(self) -> None:
+        updates = UpdateList(
+            [
+                Update([75, 47, 61, 53, 29]),
+                Update([97, 61, 53, 29, 13]),
+                Update([75, 29, 13]),
+            ]
+        )
+        assert updates.sum_of_middles == 143
 
 
-def test__sum_valid_middles() -> None:
-    updates = UpdateList(
-        [
-            Update([75, 47, 61, 53, 29]),
-            Update([97, 61, 53, 29, 13]),
-            Update([75, 29, 13]),
-        ]
-    )
-    assert updates.sum_middles == 143
+def test_part2() -> None:
+    assert part2(TEST_DATA_FILEPATH) == 123
