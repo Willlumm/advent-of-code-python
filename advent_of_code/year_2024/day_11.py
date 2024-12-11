3931b484a5948feea9c784162e3b457442b8fbe3
William Lumme <80401792+Willlumm@users.noreply.github.com>
2024-12-11 20:53:12 +0000

    feat: day 11 part 2
    - cache is OP

--- advent_of_code/year_2024/day_11.py
+++ advent_of_code/year_2024/day_11.py
@@ -1,3 +1,4 @@
+from functools import cache
 from pathlib import Path
 from time import perf_counter
 from typing import Self
@@ -10,53 +11,51 @@ def read_input(filepath: str) -> str:
         return file.read()
 
 
+@cache
+def get_next(number: int) -> list[int]:
+    if number == 0:
+        return [1]
+    number_str = str(number)
+    digit_count = len(number_str)
+    if digit_count % 2:
+        next_numbers = [number * 2024]
+    else:
+        half_length = digit_count // 2
+        next_numbers = [
+            int(number_str[:half_length]),
+            int(number_str[half_length:]),
+        ]
+    return next_numbers
+
+
+@cache
+def _simulate(number: int, i_max: int, i: int = 0) -> int:
+    next_numbers = get_next(number)
+    if i + 1 >= i_max:
+        return len(next_numbers)
+    return sum(_simulate(next_number, i_max, i + 1) for next_number in next_numbers)
+
+
 class Stones:
-    def __init__(self, stones: list[int]) -> None:
-        self.stones = stones
-        self.cache = {0: [1]}
+    def __init__(self, numbers: list[int]) -> None:
+        self.numbers = numbers
 
     @classmethod
     def from_str(cls, data: str) -> Self:
         return cls([int(n) for n in data.split()])
 
-    def get_next(self, number: int) -> list[int]:
-        if number in self.cache:
-            return self.cache[number]
-        number_str = str(number)
-        digit_count = len(number_str)
-        if digit_count % 2:
-            next_numbers = [number * 2024]
-        else:
-            half_length = digit_count // 2
-            next_numbers = [
-                int(number_str[:half_length]),
-                int(number_str[half_length:]),
-            ]
-        self.cache[number] = next_numbers
-        return next_numbers
-
-    def simulate(self, iterations: int) -> Self:
-        for _ in range(iterations):
-            self.stones = [
-                next_numbers
-                for number in self.stones
-                for next_numbers in self.get_next(number)
-            ]
-        return self
-
-    @property
-    def stone_count(self) -> int:
-        return len(self.stones)
+    def simulate(self, i_max: int) -> int:
+        return sum(_simulate(number, i_max) for number in self.numbers)
 
 
 def part1(filepath: str) -> int:
     data = read_input(filepath)
-    return Stones.from_str(data).simulate(25).stone_count
+    return Stones.from_str(data).simulate(25)
 
 
 def part2(filepath: str) -> int:
     data = read_input(filepath)
-    return Stones.from_str(data).simulate(75).stone_count
+    return Stones.from_str(data).simulate(75)
 
 
 if __name__ == "__main__":
--- tests/year_2024/test_day_11.py
+++ tests/year_2024/test_day_11.py
@@ -1,7 +1,22 @@
-from advent_of_code.year_2024.day_11 import part1
+from collections.abc import Callable
+from typing import Any
+
+from advent_of_code.year_2024.day_11 import Stones, part1, part2
 
 TEST_DATA_FILEPATH = "tests/data/2024_11"
 
 
 def test_part1() -> None:
     assert part1(TEST_DATA_FILEPATH) == 55312
+
+
+def test_part2() -> None:
+    assert part2(TEST_DATA_FILEPATH) == 65601038650482
+
+
+def test_performance_25(benchmark: Callable[..., Any]) -> None:
+    benchmark(Stones([125, 17]).simulate, 25)
+
+
+def test_performance_75(benchmark: Callable[..., Any]) -> None:
+    benchmark(Stones([125, 17]).simulate, 75)
