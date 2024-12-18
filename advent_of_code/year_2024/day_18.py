4bbac2fd90cea3b0c506ba600b2e2521bfa81951
William Lumme <80401792+Willlumm@users.noreply.github.com>
2024-12-18 20:13:27 +0000

    feat: day 18 part 2

--- advent_of_code/year_2024/day_18.py
+++ advent_of_code/year_2024/day_18.py
@@ -17,37 +17,47 @@ class MemorySpace:
         self.byte_count = byte_count
         self.xy = 0, 0
         self.exit = width - 1, height - 1
-        self.corrupted: set[tuple[int, int]] = set()
+        self.bytes: list[tuple[int, int]] = []
+        self.corrupted_bytes: set[tuple[int, int]] = set()
+        self.path: set[tuple[int, int]] = set()
 
     def parse_input(self, data: str) -> Self:
-        self.corrupted = set(
-            [
-                (int(x), int(y))
-                for x, y in (line.split(",") for line in data.split("\n"))
-            ][: self.byte_count]
-        )
+        self.bytes = [
+            (int(x), int(y)) for x, y in (line.split(",") for line in data.split("\n"))
+        ]
+        self.corrupted_bytes = set(self.bytes[: self.byte_count])
         return self
 
     def get_shortest_path(self) -> int | None:
-        xys = [(0, 0)]
+        paths = [[(0, 0)]]
         visited = {(0, 0)}
-        steps = 0
-        while xys:
-            new_xys = []
-            steps += 1
-            for x, y in xys:
+        while paths:
+            new_paths = []
+            for path in paths:
+                x, y = path[-1]
                 for x_new, y_new in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
                     if (
                         not self.width > x_new >= 0
                         or not self.height > y_new >= 0
-                        or (x_new, y_new) in visited | self.corrupted
+                        or (x_new, y_new) in visited | self.corrupted_bytes
                     ):
                         continue
                     if (x_new, y_new) == self.exit:
-                        return steps
-                    new_xys.append((x_new, y_new))
+                        self.path = {*path, (x_new, y_new)}
+                        return len(self.path) - 1
+                    new_paths.append([*path, (x_new, y_new)])
                     visited.add((x_new, y_new))
-            xys = new_xys
+            paths = new_paths
+        return None
+
+    def get_blocking_byte(self) -> tuple[int, int] | None:
+        self.get_shortest_path()
+        for xy in self.bytes[self.byte_count :]:
+            self.corrupted_bytes.add(xy)
+            if xy in self.path:
+                shortest_path_len = self.get_shortest_path()
+                if shortest_path_len is None:
+                    return xy
         return None
 
 
@@ -61,8 +71,23 @@ def part1(filepath: str, width: int, height: int, byte_count: int) -> int:
     return shortest_path
 
 
+def part2(filepath: str, width: int, height: int, byte_count: int) -> str:
+    data = read_input(filepath)
+    blocking_byte = (
+        MemorySpace(width, height, byte_count).parse_input(data).get_blocking_byte()
+    )
+    if blocking_byte is None:
+        raise ValueError
+    return ",".join(str(n) for n in blocking_byte)
+
+
 if __name__ == "__main__":
     start = perf_counter()
-    result = part1(INPUT_FILEPATH, 71, 71, 1024)
+    result1 = part1(INPUT_FILEPATH, 71, 71, 1024)
     seconds = perf_counter() - start
-    print(f"Part 1: {result:>20} {seconds:>20.1f}s")
+    print(f"Part 1: {result1:>20} {seconds:>20.1f}s")
+
+    start = perf_counter()
+    result2 = part2(INPUT_FILEPATH, 71, 71, 1024)
+    seconds = perf_counter() - start
+    print(f"Part 2: {result2:>20} {seconds:>20.1f}s")
--- tests/year_2024/test_day_18.py
+++ tests/year_2024/test_day_18.py
@@ -1,5 +1,9 @@
-from advent_of_code.year_2024.day_18 import part1
+from advent_of_code.year_2024.day_18 import part1, part2
 
 
 def test_part1() -> None:
     assert part1("tests/data/2024_18", 7, 7, 12) == 22
+
+
+def test_part2() -> None:
+    assert part2("tests/data/2024_18", 7, 7, 12) == "6,1"
